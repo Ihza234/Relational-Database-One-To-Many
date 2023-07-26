5bd4945913805dd72fca3606d6bcf50c44a2e88b
Ihza Muchlis Maulana <106130845+Ihza234@users.noreply.github.com>
2023-07-26 15:27:21 +0700

    Add files via upload

---
+++ Relation-One-To-Many/src/main/java/com/javatechie/springdatajpa/dto/OrderRequest.java
@@ -0,0 +1,15 @@
+package com.javatechie.springdatajpa.dto;
+
+import com.javatechie.springdatajpa.entity.Customer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class OrderRequest {
+    private Customer customer;
+}
---
+++ Relation-One-To-Many/src/main/java/com/javatechie/springdatajpa/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.javatechie.springdatajpa.repository;
+
+import com.javatechie.springdatajpa.dto.OrderResponse;
+import com.javatechie.springdatajpa.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+@Query("SELECT new com.javatechie.springdatajpa.dto.OrderResponse(c.name , p.productName) FROM Customer c JOIN c.products p")
+    public List<OrderResponse> getJoinInformation();
+
+}
